b5be858bff793daf840fd4ff302126039f63d8ec
vbucode <61677060+vbucode@users.noreply.github.com>
2022-04-21 19:06:49 +0500

    Add files via upload

--- translate.py
+++ translate.py
@@ -16,23 +16,37 @@ class Translate:
     def __init__(self, text):
         self.text = text
     def load(self):
+        var = 0
         for i in self.text:
             count = 0
-            for j in llist:
-                count += 1
-                clearlist = []
-                clearlist = re.split("[\-\s]", j)
-                if clearlist[0] == i and len(clearlist) == 1 and len(self.text) == 1:
-                    tlist.append(rlist[llist.index(j)])
-                    break
-                elif clearlist[0] == i and len(clearlist) > 1 and len(self.text) > 1:
-                    count2 = 0
-                    for k in clearlist:
-                        if k in self.text:
-                            count2 += 1
-                    if count2 == len(clearlist):
+            while var > 0:
+                var -= 1
+                break
+            else:
+                for j in llist:
+                    count += 1
+                    clearlist = []
+                    clearlist = re.split("[\-\s]", j)
+                    if clearlist[0] == i and len(clearlist) == 1 and len(self.text) == 1:
                         tlist.append(rlist[llist.index(j)])
                         break
-                elif count == len(llist):
-                    tlist.append("out")
+                    elif clearlist[0] == i and len(clearlist) == 1 and len(self.text) > 1:
+                        tlist.append(rlist[llist.index(j)])
+                        break
+                    elif clearlist[0] == i and len(clearlist) > 1 and len(self.text) > 1:
+                        try:
+                            count2 = 0
+                            for k in clearlist:
+                                if k == self.text[self.text.index(i) + count2]:
+                                    count2 += 1
+                                else:
+                                    tlist.append("out")
+                            if count2 == len(clearlist):
+                                tlist.append(rlist[llist.index(j)])
+                                var = len(clearlist) - 1
+                        except IndexError:
+                            tlist.append("out")
+                        break
+                    elif count == len(llist):
+                        tlist.append("out")
         return tlist
