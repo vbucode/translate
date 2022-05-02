5eb86e6595805fbb8202309ca89f2effc488243a
vbucode <61677060+vbucode@users.noreply.github.com>
2022-05-02 21:36:24 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from search import Search
 from words import Words
 
-def main():
-    inp = input("translate: ")
-    if inp == "exit":
-        exit()
-    w = Words(inp)
-    wl = w.load()
-    s = Search(wl)
-    sl = s.load()
-    varstring = " ".join(sl)
-    #sl.clear()
-    print("translated text: ", sl)
-while True:
-    main()
+inp = input("translate: ")
+if inp == "exit":
+    exit()
+w = Words(inp)
+wl = w.load()
+s = Search(wl)
+sl = s.load()
+vs = " ".join(sl)
+print("translated text: ", vs)
