17e60af688074e9881ca6b650d9b833bcce6a608
vbucode <61677060+vbucode@users.noreply.github.com>
2022-05-20 20:33:33 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import messagebox
 from tkinter import scrolledtext
 from translate import Translate
 from words import Words
@@ -23,14 +24,19 @@ def deletef():
     txt.delete(1.0, tkinter.END)
     txt2.delete(1.0, tkinter.END)
 
+def infof():
+    msg = messagebox.showinfo( "Translate", "by vbucode")
+
 btn1 = tkinter.Button(root, text = "translate", bg = "gray", command = translatef)
 btn2 = tkinter.Button(root, text = "delete", bg = "gray", command = deletef)
+btn3 = tkinter.Button(root, text = "info", bg = "gray", command = infof)
 txt = scrolledtext.ScrolledText(root, width = 35, height = 10)
 txt2 = scrolledtext.ScrolledText(root, width = 35, height = 10)
 txt.place(x = 0, y = 0)
 txt2.place(x = 300, y = 0)
 btn1.place(x = 5, y = 185)
-btn2.place(x = 160, y = 185)
+btn2.place(x = 96, y = 185)
+btn3.place(x = 170, y = 185)
 
 if __name__ == "__main__":
     root.mainloop()
